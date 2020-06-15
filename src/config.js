66f955127b2888ddf7ff696b6664b942faaaadf3
Jonathan Moore <jonathan.moore6247@gmail.com>
2020-06-15 05:03:51 -0500

    Setting Up Our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-app-uploads-jonathan-moore"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ozpocytvyi.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_lWuDSTD9X",
+    APP_CLIENT_ID: "dta1qmlj2qb6a0pgluh7ol2mn",
+    IDENTITY_POOL_ID: "us-east-2:3b53d3e3-e8ee-4a32-8d23-3db42972779e"
+  }
+};
