26bff89f93cb3e0f2f6b7295a330cca0476f31fd
Jonathan Moore <jonathan.moore6247@gmail.com>
2020-06-15 07:03:50 -0500

    Completed login and signup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Amplify } from 'aws-amplify';
-import config from './containers';
+import config from './config';
 
 Amplify.configure({
   Auth: {
